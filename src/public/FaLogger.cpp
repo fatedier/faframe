e624899381be6056622bb73f865662f988529989
fatedier <512654112@qq.com>
2015-05-11 16:50:13 +0800

    新增日志文件可按照文件大小切分的功能

--- src/public/FaLogger.cpp
+++ src/public/FaLogger.cpp
@@ -12,45 +12,95 @@
 FaLogger::FaLogger()
 {
     m_nLogLevel = FADEBUG;
+    m_nMaxBackupIndex = 1;
+    m_nMaxFileSize = 1;
+    m_nNowBackupNum = 0;
+    m_nRotateWay = FANONE;
 }
 
-//构造函数：需要配置文件对象、配置的前缀名（如 test.log.level，test就是前缀名）
-FaLogger::FaLogger(FaProperties& cfgFile, const char* preName)
+// 构造函数：需要配置文件对象、配置的前缀名（如 test.log.level，test就是前缀名）
+FaLogger::FaLogger(FaProperties& cfgFile, const string& strPreName)
 {
-    this->init(cfgFile, preName);
+    this->init(cfgFile, strPreName);
+}
+
+// 构造函数：需要配置文件路径、配置的前缀名
+FaLogger::FaLogger(const string& cfgFilePath, const string& strPreName)
+{
+    this->init(cfgFilePath, strPreName);
 }
 
 FaLogger::~FaLogger()
 {
-
 }
 
 
-//初始化
-void FaLogger::init(FaProperties& cfgFile, const char* preName)
+// 通过FaProperties对象初始化
+void FaLogger::init(FaProperties& cfgFile, const string& strPreName)
 {
-    string strPreName = preName;
-    if (strPreName != "")
+    string strPreNameFull = strPreName;
+    if (strPreNameFull != "")
     {
-        strPreName += ".";
+        strPreNameFull += ".";
     }
-    //取日志级别
-    m_nLogLevel = cfgFile.getInt(strPreName + "log.level", FADEBUG);
-    //取日志文件名
-    m_strLogFile = cfgFile.getString(strPreName + "log.filename", "./logs/default.log");
+    // 取日志级别
+    m_nLogLevel = cfgFile.getInt(strPreNameFull + "log.level", FADEBUG);
+    // 取日志文件名
+    m_strLogFile = cfgFile.getString(strPreNameFull + "log.filename", "./logs/default.log");
+    // 取日志切分方式
+    string strRotateWay = cfgFile.getString(strPreNameFull + "log.rotateway", "size");
+    if (strRotateWay == "size")
+    {
+        m_nRotateWay = FASIZE;
+        m_nMaxFileSize = cfgFile.getInt(strPreNameFull + "log.max_file_size", 1) * 1024 * 1024;
+    }
+    else if (strRotateWay == "day")
+    {
+        m_nRotateWay = FADAY;
+    }
+    // 取最多保留的日志个数
+    m_nMaxBackupIndex = cfgFile.getInt(strPreNameFull + "log.max_backup_index", 1);
 
-    //初始化日志文件对象
+    string dirName = fa_getDirOfFile(m_strLogFile);
+    if (dirName != "")
+    {
+        if (!fa_fileExists(dirName))
+        {
+            if (mkdir(dirName.c_str(), 0755) != 0)
+            {
+                string e = "创建目录 [" + dirName + "] 失败！";
+                throw FaException(ERROR_FILE, e);
+            }
+        }
+    }
+    // 初始化日志文件对象
     m_fileOut = fopen(m_strLogFile.c_str(), "a+");
+    if (m_fileOut == NULL)
+    {
+        string e = "创建日志文件 [" + m_strLogFile + "] 失败！";
+        throw FaException(ERROR_FILE, e);
+    }
+
+    m_nNowBackupNum = 0;
 }
 
-//关闭文件输出对象
+// 通过配置文件路径进行初始化
+void FaLogger::init(const string& cfgFilePath, const string& strPreName)
+{
+    FaProperties cfgFile;
+    cfgFile.open(cfgFilePath.c_str());
+    this->init(cfgFile, strPreName);
+    cfgFile.close();
+}
+
+// 关闭文件输出对象
 void FaLogger::close()
 {
     fclose(m_fileOut);
     m_fileOut = NULL;
 }
 
-//检测指定日志级别是否能够输出
+// 检测指定日志级别是否能够输出
 bool FaLogger::isEnableFor(FaLogLevel logLevel)
 {
     if (logLevel >= m_nLogLevel)
@@ -63,7 +113,7 @@ bool FaLogger::isEnableFor(FaLogLevel logLevel)
     }
 }
 
-//记录一条日志
+// 记录一条日志
 void FaLogger::log(FaLogLevel logLevel, const string& msg, const char* file, int line)
 {
     if (false == this->isEnableFor(logLevel))
@@ -71,6 +121,7 @@ void FaLogger::log(FaLogLevel logLevel, const string& msg, const char* file, int
         return;
     }
 
+    // 拼接日志
     string strNow = fa_getSysTime();
     stringstream os;
     os << "[";
@@ -83,5 +134,54 @@ void FaLogger::log(FaLogLevel logLevel, const string& msg, const char* file, int
     os << "[" << file << ":";               //代码所在文件名
     os << line << "]\n";                    //代码所在行
 
+    pthread_mutex_lock(&m_lock);
+
+    // 检查日志文件大小是否需要切分
+    if (m_nRotateWay == FASIZE)
+    {
+        int nFileSize = ftell(m_fileOut);
+        // 当前文件大小已经超出设置值，进行文件切分
+        if (nFileSize >= m_nMaxFileSize)
+        {
+            if (m_nNowBackupNum >= m_nMaxBackupIndex)
+            {
+                unlink((m_strLogFile + "." + fa_ltostr(m_nNowBackupNum)).c_str());
+                m_nNowBackupNum--;
+            }
+            for (int i=m_nNowBackupNum; i>0; i--)
+            {
+                string strOldFile = m_strLogFile + "." + fa_ltostr(i);
+                string strNewFile = m_strLogFile + "." + fa_ltostr(i + 1);
+                rename(strOldFile.c_str(), strNewFile.c_str());
+            }
+            this->close();
+            rename(m_strLogFile.c_str(), (m_strLogFile + ".1").c_str());
+
+            string dirName = fa_getDirOfFile(m_strLogFile);
+            if (dirName != "")
+            {
+                if (!fa_fileExists(dirName))
+                {
+                    if (mkdir(dirName.c_str(), 0755) != 0)
+                    {
+                        string e = "创建目录 [" + dirName + "] 失败！";
+                        throw FaException(ERROR_FILE, e);
+                    }
+                }
+            }
+            
+            // 重新生成一个日志文件
+            m_fileOut = fopen(m_strLogFile.c_str(), "a+");
+            if (m_fileOut == NULL)
+            {
+                string e = "创建日志文件 [" + m_strLogFile + "] 失败！";
+                throw FaException(ERROR_FILE, e);
+            }
+            m_nNowBackupNum++;
+            pthread_mutex_unlock(&m_lock);
+        }
+    }
+
     fprintf(m_fileOut, "%s", os.str().c_str());
+    pthread_mutex_unlock(&m_lock);
 }
--- src/public/FaLogger.h
+++ src/public/FaLogger.h
@@ -10,20 +10,31 @@
 #ifndef FAFRAME_PUBLIC_FALOGGER_H_
 #define FAFRAME_PUBLIC_FALOGGER_H_
 
+#include <pthread.h>
+#include <sstream>
+#include <limits>
 #include "FaProperties.h"
 #include "FaFunc.h"
 
-//日志等级定义
+// 日志等级定义
 enum FaLogLevel
 {
-    FAFATAL,    //0
-    FAERROR,    //1
-    FAWARN,     //2
-    FAINFO,     //3
-    FADEBUG     //4
+    FAFATAL,    // 0
+    FAERROR,    // 1
+    FAWARN,     // 2
+    FAINFO,     // 3
+    FADEBUG     // 4
 };
 
-//日志等级对应的字符串
+// 日志切分方式定义
+enum FaLogRotateWay
+{
+    FANONE,     // 0
+    FASIZE,     // 1
+    FADAY       // 2
+};
+
+// 日志等级对应的字符串
 const char sLogLevelName[][10] =
 {
     "FATAL",
@@ -33,47 +44,62 @@ const char sLogLevelName[][10] =
     "DEBUG"
 };
 
-//提前声明
+// 提前声明
 class FaLogger;
 
-//日志函数宏定义
+// 日志函数宏定义
 #define LOG_FATAL(logger, strMsg)\
 {\
-    if(true == (logger)->isEnableFor(FAFATAL))\
+    if (true == (logger)->isEnableFor(FAFATAL))\
     {\
-        (logger)->log(FAFATAL, strMsg, __FILE__, __LINE__);\
+        ostringstream os;\
+        os.precision(numeric_limits<double>::digits10 + 1);\
+        os << strMsg;\
+        (logger)->log(FAFATAL, os.str(), __FILE__, __LINE__);\
     }\
 }\
 
 #define LOG_ERROR(logger, strMsg)\
 {\
-    if(true == (logger)->isEnableFor(FAERROR))\
+    if (true == (logger)->isEnableFor(FAERROR))\
     {\
-        (logger)->log(FAERROR, strMsg, __FILE__, __LINE__);\
+        ostringstream os;\
+        os.precision(numeric_limits<double>::digits10 + 1);\
+        os << strMsg;\
+        (logger)->log(FAERROR, os.str(), __FILE__, __LINE__);\
     }\
 }\
 
 #define LOG_WARN(logger, strMsg)\
 {\
-    if(true == (logger)->isEnableFor(FAWARN))\
+    if (true == (logger)->isEnableFor(FAWARN))\
     {\
-        (logger)->log(FAWARN, strMsg, __FILE__, __LINE__);\
+        ostringstream os;\
+        os.precision(numeric_limits<double>::digits10 + 1);\
+        os << strMsg;\
+        (logger)->log(FAWARN, os.str(), __FILE__, __LINE__);\
     }\
 }\
 
 #define LOG_INFO(logger, strMsg)\
 {\
-    if(true == (logger)->isEnableFor(FAINFO))\
+    if (true == (logger)->isEnableFor(FAINFO))\
     {\
-        (logger)->log(FAINFO, strMsg, __FILE__, __LINE__);\
+        ostringstream os;\
+        os.precision(numeric_limits<double>::digits10 + 1);\
+        os << strMsg;\
+        (logger)->log(FAINFO, os.str(), __FILE__, __LINE__);\
     }\
 }\
 
 #define LOG_DEBUG(logger, strMsg)\
 {\
-    if(true == (logger)->isEnableFor(FADEBUG))\
+    if (true == (logger)->isEnableFor(FADEBUG))\
     {\
-        (logger)->log(FADEBUG, strMsg, __FILE__, __LINE__);\
+        ostringstream os;\
+        os.precision(numeric_limits<double>::digits10 + 1);\
+        os << strMsg;\
+        (logger)->log(FADEBUG, os.str(), __FILE__, __LINE__);\
     }\
 }\
 
@@ -82,20 +108,31 @@ class FaLogger;
 * 先调用 init 函数从配置文件初始化日志对象
 * 在需要打印日志的代码中使用 LOG_XXXX(logger, strMsg)，logger为已经初始化的日志对象
 * 结束时调用 close函数
+*
+* 需要从配置文件读取日志配置信息，xxx为前缀名，这样可以在同一个进程中配置多个日志对象
+* xxx.log.level: 日志级别
+* xxx.log.filename: 日志文件名
+* xxx.log.rotateway: 日志切分方式 size(按文件大小) day(按天)
+* xxx.log.max_backup_index: 最多保留的日志个数
+* xxx.log.max_file_size: 如果选择按size切分，需要设置日志文件大小，单位MB
 */
 class FaLogger
 {
 public:
     FaLogger();
-    //构造函数：需要配置文件对象、配置的前缀名（如 test.log.level，test就是前缀名）
-    FaLogger(FaProperties& cfgFile, const char* preName);
+    // 构造函数：需要配置文件对象、配置的前缀名（如 test.log.level，test就是前缀名）
+    FaLogger(FaProperties& cfgFile, const string& strPreName);
+    // 构造函数：需要配置文件路径、配置的前缀名
+    FaLogger(const string& cfgFilePath, const string& strPreName);
     virtual ~FaLogger();
 
-    //初始化
-    void init(FaProperties& cfgFile, const char* preName);
-    //关闭文件输出对象
+    // 通过FaProperties对象初始化
+    void init(FaProperties& cfgFile, const string& strPreName);
+    // 通过配置文件路径进行初始化
+    void init(const string& cfgFilePath, const string& strPreName);
+    // 关闭文件输出对象
     void close();
-    //检测指定日志级别是否能够输出
+    // 检测指定日志级别是否能够输出
     bool isEnableFor(FaLogLevel logLevel);
 
     /*
@@ -108,9 +145,16 @@ public:
     void log(FaLogLevel logLevel, const string& msg, const char* file, int line);
 
 private:
-    int m_nLogLevel;        //日志级别
-    string m_strLogFile;    //日志文件
-    FILE* m_fileOut;        //文件输出对象
+    pthread_mutex_t m_lock; // 互斥锁
+
+    int m_nLogLevel;        // 日志级别
+    string m_strLogFile;    // 日志文件
+    int m_nRotateWay;       // 日志切分方式
+    int m_nMaxBackupIndex;  // 最多保留的日志个数
+    int m_nMaxFileSize;     // 如果按文件大小切分，切分文件的阀值，单位为Byte
+    FILE* m_fileOut;        // 文件输出对象
+
+    int m_nNowBackupNum;    // 当前存在的日志个数
 };
 
 #endif //FAFRAME_PUBLIC_FALOGGER_H_
